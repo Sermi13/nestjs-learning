22bd548d486860c8a16a9ebe09fad901d44454c5
Enrico Bordin <enrico.xavier@santosvahlisgroup.com>
2024-11-24 23:14:47 -0300

    UserEntity - getter methods unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-  testEnvironment: "node",
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { readFileSync } from 'fs';
+
+const tsconfig = JSON.parse(readFileSync('./tsconfig.json', 'utf-8'));
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  testEnvironment: 'node',
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    '^.+\\.tsx?$': ['ts-jest', {}], // Matches TypeScript files
   },
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
 };
