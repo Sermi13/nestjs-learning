20ab841a316ccd661d4670392da9d1bc377f6332
Enrico Bordin <enrico.xavier@santosvahlisgroup.com>
2024-11-26 15:59:45 -0300

    Added unit tests to class validator part 1

---
+++ src/shared/domain/validators/__tests__/unit/class-validator-fields.spec.ts
@@ -0,0 +1,28 @@
+import { ClassValidatorFields } from '../../class-validator-fields';
+import * as libClassValidator from 'class-validator';
+
+class StubClassValidatorFields extends ClassValidatorFields<{
+  field: string;
+}> {}
+
+describe('ClassValidatorFields unit test', () => {
+  it('Should initialize errors and validatedData variables with null', () => {
+    const sut = new StubClassValidatorFields();
+    expect(sut.errors).toBeNull();
+    expect(sut.validatedData).toBeNull();
+  });
+
+  it('Should validate with errors', () => {
+    const spyValidatedSync = jest.spyOn(libClassValidator, 'validateSync');
+    spyValidatedSync.mockReturnValue([
+      { property: 'field', constraints: { isRequired: 'test error' } },
+    ]);
+
+    const sut = new StubClassValidatorFields();
+
+    expect(sut.validate(null)).toBeFalsy();
+    expect(spyValidatedSync).toHaveBeenCalled();
+    expect(sut.validatedData).toBeNull();
+    expect(sut.errors).toStrictEqual({ field: ['test error'] });
+  });
+});
